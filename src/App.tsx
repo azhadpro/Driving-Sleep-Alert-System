9ff10de9b580a8a87fcc20f206f2f32954fd20c5
Tuan Azhad <azhad.ty@gmail.com>
2026-02-24 23:04:15 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import DrowsinessMonitor from './components/DrowsinessMonitor';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#E6E6E6]">
+      <DrowsinessMonitor />
+    </div>
+  );
+}
